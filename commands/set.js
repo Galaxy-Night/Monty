c2aefe9e0f80a063a81e0e97c68bf76bf5a26ee3
Galaxy-Night <meliaerinelizabeth@gmail.com>
2022-06-08 10:22:31 -0700

    rewrote set so all information is set from one command

--- commands/set.js
+++ commands/set.js
@@ -1,28 +1,37 @@
-const fs = require('node:fs');
-
-const subcommands = new Map();
-const subcommandFiles = fs.readdirSync('./commands/set');
-
-for (const file of subcommandFiles) {
-	const subcommand = require(`./set/${file}`);
-	subcommands.set(subcommand.name, subcommand);
-}
-
 module.exports = {
 	name: 'set',
 	async execute(interaction, db) {
-		const key = interaction.options.getSubcommandGroup(false) === null ? interaction.options.getSubcommand() :
-			interaction.options.getSubcommandGroup();
-		const subcommand = subcommands.get(key);
-		if (!subcommand) return;
+		const collection = db.collection(String(interaction.guildId));
+		const name = interaction.options.getString('name');
 
-		try {
-			await subcommand.execute(interaction, db);
+		const setObject = {
+			class: interaction.options.getString('class'),
+			'ratings.charm': interaction.options.getInteger('charm'),
+			'ratings.cool': interaction.options.getInteger('cool'),
+			'ratings.sharp': interaction.options.getInteger('sharp'),
+			'ratings.tough': interaction.options.getInteger('tough'),
+			'ratings.weird': interaction.options.getInteger('weird'),
+			luck: interaction.options.getInteger('luck'),
+			harm: interaction.options.getInteger('harm'),
+			xp: interaction.options.getInteger('experience')
+		};
+
+		// remove empty keys from setObject
+		Object.keys(setObject).forEach(key => setObject[key] == null && delete setObject[key]);
+
+		const character = await collection.findOne({ nameLower: name.toLowerCase() });
+
+		if (character === null) {
+			await interaction.reply({ content: `I couldn't find anyone named ${name}!`, ephemeral: true });
+			return;
 		}
-		catch (e) {
-			console.error(e);
-			await interaction.reply({ content: 'Something went wrong and I couldn\'t execute that command!',
-				ephemeral: true });
-		}
+
+		const result = await collection.updateOne({ nameLower: name.toLowerCase() },
+			{ $set: setObject });
+
+		if (result.updatedCount === 0)
+			await interaction.reply(`Something went wrong and I couldn't update ${name}'s ability scores!`);
+		else
+			await interaction.reply(`${name}'s ability scores have been updated!`);
 	}
 };
